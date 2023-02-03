27b9233e6ba157ecf6fdc6636f887b9368ca07e3
monil03 <60208695+monil03@users.noreply.github.com>
2023-02-03 14:53:12 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,4 +1,8 @@
 $username = $_POST['username'];
 $password = $_POST['password'];
+$adminFlag = $_POST['isAdmin'];
 $query = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+if ($adminFlag) {
+  $query .= " AND isAdmin = 1";
+}
 $result = mysqli_query($connection, $query);
